f9cedb64e8afaea41ae325a5274c8f5233fa79f8
m-mfp <mmspontes@gmail.com>
2025-03-26 15:37:51 +0100

    Added ugly but working play button

--- MineSweepGame/main.py
+++ MineSweepGame/main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 import settings, utils
 from cell import Cell
 
@@ -8,39 +9,51 @@ class App(tk.Tk):
         self.configure(bg="black")
         self.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
         self.title("MineSweep Game")
-        #self.resizable(False, False)
 
-        top_frame = tk.Frame(
+        self.top_frame = tk.Frame(
             self,
             bg = "black",
             width = settings.WIDTH,
             height = utils.height_prct(25),
         )
-        top_frame.place(x=0, y=0)
+        self.top_frame.place(x=0, y=0)
 
-        left_frame = tk.Frame(
+        self.left_frame = tk.Frame(
             self,
             bg = "black",
             width = utils.width_prct(25),
             height = utils.height_prct(75),
         )
-        left_frame.place(x=0, y=utils.height_prct(25))
+        self.left_frame.place(x=0, y=utils.height_prct(25))
 
-        center_frame = tk.Frame(
+        self.center_frame = tk.Frame(
             self,
             bg = "black",
             width = utils.width_prct(75),
             height = utils.height_prct(75),
         )
-        center_frame.place(x=utils.width_prct(25), y=utils.height_prct(25))
+        self.center_frame.place(x=utils.width_prct(25), y=utils.height_prct(25))
 
+        self.start_menu()    
+
+    def start_menu(self):
+        self.playbutton = tk.Button(
+            self.top_frame,
+            text='PLAY',
+            bg='grey',
+            fg='ivory',
+            command=self.start_game,
+        )
+        self.playbutton.pack(expand=True)
+
+    def start_game(self):
         for x in range(settings.GRID_SIZE):
             for y in range(settings.GRID_SIZE):
                 c = Cell(x, y)
-                c.create_btn_object(center_frame)
+                c.create_btn_object(self.center_frame)
                 c.cell_btn_object.grid(column=x, row=y)
 
-        Cell.create_cell_count_label(left_frame)
+        Cell.create_cell_count_label(self.left_frame)
         Cell.cell_count_label_object.place(x=0, y=0)
         Cell.randomize_mines()
 
