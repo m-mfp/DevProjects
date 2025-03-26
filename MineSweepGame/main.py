5c53570cc4084bfebbab749f4437bae19c4ea80e
m-mfp <mmspontes@gmail.com>
2025-03-26 14:35:05 +0100

    changed app to a class.

--- MineSweepGame/main.py
+++ MineSweepGame/main.py
@@ -1,57 +1,49 @@
-from tkinter import Tk, Frame, Button, Label
+import tkinter as tk
 import settings, utils
 from cell import Cell
 
-root = Tk()
-root.configure(bg="black")
-root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
-root.title("MineSweep Game")
-root.resizable(False, False)
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.configure(bg="black")
+        self.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
+        self.title("MineSweep Game")
+        #self.resizable(False, False)
 
-top_frame = Frame(
-    root,
-    bg = "black",
-    width = settings.WIDTH,
-    height = utils.height_prct(25),
-)
-top_frame.place(x=0, y=0)
+        top_frame = tk.Frame(
+            self,
+            bg = "black",
+            width = settings.WIDTH,
+            height = utils.height_prct(25),
+        )
+        top_frame.place(x=0, y=0)
 
-game_title = Label(
-    top_frame,
-    bg='black',
-    fg='white',
-    text='MineSweep Game',
-    font=('', 48)
-)
-game_title.place(
-    x=utils.width_prct(25),
-    y=0
-)
+        left_frame = tk.Frame(
+            self,
+            bg = "black",
+            width = utils.width_prct(25),
+            height = utils.height_prct(75),
+        )
+        left_frame.place(x=0, y=utils.height_prct(25))
 
-left_frame = Frame(
-    root,
-    bg = "black",
-    width = utils.width_prct(25),
-    height = utils.height_prct(75),
-)
-left_frame.place(x=0, y=utils.height_prct(25))
+        center_frame = tk.Frame(
+            self,
+            bg = "black",
+            width = utils.width_prct(75),
+            height = utils.height_prct(75),
+        )
+        center_frame.place(x=utils.width_prct(25), y=utils.height_prct(25))
 
-center_frame = Frame(
-    root,
-    bg = "black",
-    width = utils.width_prct(75),
-    height = utils.height_prct(75),
-)
-center_frame.place(x=utils.width_prct(25), y=utils.height_prct(25))
+        for x in range(settings.GRID_SIZE):
+            for y in range(settings.GRID_SIZE):
+                c = Cell(x, y)
+                c.create_btn_object(center_frame)
+                c.cell_btn_object.grid(column=x, row=y)
 
-for x in range(settings.GRID_SIZE):
-    for y in range(settings.GRID_SIZE):
-        c = Cell(x, y)
-        c.create_btn_object(center_frame)
-        c.cell_btn_object.grid(column=x, row=y)
+        Cell.create_cell_count_label(left_frame)
+        Cell.cell_count_label_object.place(x=0, y=0)
+        Cell.randomize_mines()
 
-Cell.create_cell_count_label(left_frame)
-Cell.cell_count_label_object.place(x=0, y=0)
-Cell.randomize_mines()
-
-root.mainloop()
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
