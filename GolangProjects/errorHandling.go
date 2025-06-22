ada63347671684e36adec690ac0c7f18856a7d17
m-mfp <mmspontes@gmail.com>
2025-06-22 13:41:10 +0200

    Basic error handling

---
+++ GolangProjects/errorHandling.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+}
+
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return x, ErrNegativeSqrt(x)
+	}
+	return math.Sqrt(x), nil
+}
+
+func main() {
+	result, err := Sqrt(2)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(result)
+	}
+}
