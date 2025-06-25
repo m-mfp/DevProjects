cb78af52c5729eccc5df189db8480a2396193571
m-mfp <mmspontes@gmail.com>
2025-06-25 16:39:35 +0200

    function create potion

--- GolangProjects/skyrimAlchemyScrapper/main.go
+++ GolangProjects/skyrimAlchemyScrapper/main.go
@@ -24,43 +24,51 @@ func main() {
 	if len(os.Args) < 2 {
 		log.Fatal("Please provide an ingredient name or effect as a command-line argument.")
 	} else {
-		if arg == "-p" || arg == "-potion" {
-			fmt.Println("Creating potion...")
-			arg = strings.ToLower(os.Args[2])
+		if os.Args[1] == "-p" || os.Args[1] == "-potion" {
+			args := os.Args[2:]
+			potionIngredients, err := createPotion(args)
+			if err != nil {
+				log.Fatalf("Error with createPotions: %v", err)
+			}
+
+			fmt.Println("Potion Ingredients are:")
+			for _, ingredient := range potionIngredients {
+				fmt.Println(ingredient)
+			}
 		} else {
 			arg = strings.ToLower(os.Args[1])
-		}
-	}
 
-	effects, err := getEffects(arg)
-	if err != nil {
-		log.Fatalf("Error with getEffects: %v", err)
-	} else if effects != nil {
-		fmt.Printf("Effects for %s:\n", arg)
-		for _, effect := range effects {
-			fmt.Printf("- %s\n", effect)
-		}
+			effects, err := getEffects(arg)
+			if err != nil {
+				log.Fatalf("Error with getEffects: %v", err)
+			} else if effects != nil {
+				fmt.Printf("Effects for %s:\n", arg)
+				for _, effect := range effects {
+					fmt.Printf("- %s\n", effect)
+				}
 
-		combinations, err := getCombinations(effects)
-		if err != nil {
-			log.Fatalf("Error with getCombinations: %v", err)
-		}
-		fmt.Println("\nCombinations:")
-		for effect, ingredients := range combinations {
-			fmt.Printf("\n-%s:\n", effect)
-			for _, ingredient := range ingredients {
-				fmt.Printf("-- %s\n", ingredient)
+				combinations, err := getCombinations(effects)
+				if err != nil {
+					log.Fatalf("Error with getCombinations: %v", err)
+				}
+				fmt.Println("\nCombinations:")
+				for effect, ingredients := range combinations {
+					fmt.Printf("\n-%s:\n", effect)
+					for _, ingredient := range ingredients {
+						fmt.Printf("-- %s\n", ingredient)
+					}
+				}
 			}
-		}
-	}
 
-	ingredients, err := getIngredients(arg)
-	if err != nil {
-		log.Fatalf("Error with getIngredients: %v", err)
-	} else if ingredients != nil {
-		fmt.Printf("Ingredients for %s:\n", arg)
-		for _, ingredient := range ingredients {
-			fmt.Printf("- %s\n", ingredient)
+			ingredients, err := getIngredients(arg)
+			if err != nil {
+				log.Fatalf("Error with getIngredients: %v", err)
+			} else if ingredients != nil {
+				fmt.Printf("Ingredients for %s:\n", arg)
+				for _, ingredient := range ingredients {
+					fmt.Printf("- %s\n", ingredient)
+				}
+			}
 		}
 	}
 }
